e2b7560e03dcc009521144bb4d9a84107ea49210
unknown <antagirisamyuktha@gmail.com>
2024-12-28 11:18:26 +0530

    added new file

--- project/project.js
+++ project/project.js
@@ -1,8 +1,12 @@
 
 const monthNameEl = document.getElementById("month-name");
+const prevDateEl = document.getElementById("prev-date");
 const dayNameEl = document.getElementById("day-name");
+const dayNameEle = document.getElementById("day-name1");
 const dayNumEl = document.getElementById("day-number");
+const nextDateEl = document.getElementById("next-date");
 const yearEl = document.getElementById("year");
+
 
 const date = new Date();
 const month = date.getMonth();
@@ -14,6 +18,10 @@ dayNameEl.innerText = date.toLocaleString("en", {
   weekday: "long",
 });
 
+prevDateEl.innerText = previousDate.toLocaleString("en", { month: "long" ,
+  day:"numeric", year: "numeric" });
+nextDateEl.innerText = nextDate.toLocaleString("en", { month: "long",
+   day: "numeric", year: "numeric" });
+
 dayNumEl.innerText = date.getDate();
 
-yearEl.innerText = date.getFullYear();
